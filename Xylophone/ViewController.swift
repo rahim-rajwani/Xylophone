02ee78503f638e42578d369df210637e40fae47e
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2021-01-01 05:59:16 +1100

    Update ViewController.swift

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,10 +6,11 @@
 // remember option + highlighting purple brings up quick documentation for apple code
 
 import UIKit
-import AVFoundation //Audio Visual Foundation Module (Apple)
+import AVFoundation
 
-var player: AVAudioPlayer? //
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,22 +21,10 @@ class ViewController: UIViewController {
     }
     
     func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
-
 }
 
