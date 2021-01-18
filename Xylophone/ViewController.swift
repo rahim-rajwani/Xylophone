a53ee14203e41803f1bc36f5633e278d8f5a3a34
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2021-01-18 13:29:42 +1100

    Update ViewController.swift

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+    
+        print(sender.currentTitle)
         
         playSound(soundName: sender.currentTitle!)
         
